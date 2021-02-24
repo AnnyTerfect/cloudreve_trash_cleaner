1297b1fa6effd5fd37acc5d956fac464c3165987
AnnyTerfect <1247470246@qq.com>
2021-02-24 22:52:44 +0800

    added database cleaning

--- main.py
+++ main.py
@@ -20,13 +20,6 @@ def main(work_dir=''):
 
 def clean(work_dir):
 	os.chdir(work_dir)
-	conn = sqlite3.connect('cloudreve.db')
-	cursor = conn.cursor()
-	cursor.execute('DELETE FROM files WHERE instr(name, ".DS_Store")>0')
-	cursor.execute('DELETE FROM files WHERE name in (SELECT f2.name FROM files f1, files f2 WHERE f2.name="._"||f1.name)')
-	conn.commit()
-	conn.close()
-
 	files = check_output(['find', f'{work_dir}/uploads/1/']).decode().split('\n')[:-1]
 	paths = ['/'.join(file.split('/')[:-1]) for file in files]
 	names = [file.split('/')[-1] for file in files]
@@ -37,6 +30,23 @@ def clean(work_dir):
 			full_path = f'{path}/{pre_name}_{name}'
 			log(f'cleaning {full_path}')
 			check_output(['rm', full_path])
+
+	conn = sqlite3.connect('cloudreve.db')
+	cursor = conn.cursor()
+
+	cursor.execute('SELECT id, source_name FROM files;')
+	db_files = cursor.fetchall()
+	for (fid, source_name) in db_files:
+		if not(os.path.exists(source_name)):
+			cursor.execute(f'DELETE FROM files WHERE id={fid}')
+			log(f'cleaning {source_name}')
+	
+	cursor.execute('DELETE FROM files WHERE instr(name, ".DS_Store")>0')
+	cursor.execute('DELETE FROM files WHERE name in (SELECT f2.name FROM files f1, files f2 WHERE f2.name="._"||f1.name)')
+
+	conn.commit()
+	conn.close()
+
 
 if __name__ == '__main__':
 	Fire(main)
